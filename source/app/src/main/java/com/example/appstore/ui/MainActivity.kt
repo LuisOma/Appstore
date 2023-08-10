9e71196dc46ce59e651538678bcef60adfbce8b0
Luis Omar Montes Agüero <luis.montes@metafinanciera.com>
2023-08-09 18:18:08 -0600

    Se sube readme

--- source/app/src/main/java/com/example/appstore/ui/MainActivity.kt
+++ source/app/src/main/java/com/example/appstore/ui/MainActivity.kt
@@ -12,19 +12,6 @@ import com.example.appstore.databinding.ActivityMainBinding
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun initComponents() {
-       // configureNavController()
         Prefs.init(applicationContext)
     }
-
-    /**
-    private fun configureNavController() {
-        val navController =
-            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
-        binding.toolbar.setupWithNavController(
-            navController, AppBarConfiguration(setOf(R.id.navigation_home))
-        )
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-        }
-    }
-    **/
 }
